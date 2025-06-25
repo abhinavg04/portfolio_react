2a5cdc999cc567b3f3fee2340f8602eb61bb35d5
abhinavg <abhinavg1024@gmail.com>
2025-06-25 14:28:26 +0530

    skill showcase removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css'
 import Hero from './components/Hero'
 import Projects from './components/Projects/Projects'
-import SkillsShowcase from './components/Skills/SkillsShowCase'
 import ContactSection from './components/ContactSection'
 import Footer from './components/Footer'
 import About from './components/About/About'
